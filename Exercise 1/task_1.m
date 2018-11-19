6ea282aeb504f8c43edf1d221fa91ede7d06d82d
Cristina Serrano González <serranog.cristina@gmail.com>
2018-11-19 19:28:48 +0100

    Delete unpointed rows

--- Exercise 1/task_1.m
+++ Exercise 1/task_1.m
@@ -16,7 +16,7 @@ path_images = 'data/data/images/init_texture'; % Path to the images folder
 dir_images = dir(fullfile(path_images,'*.jpg')); % Select .JPG files
 num_images = length(dir_images); % Number of images in folder
 
-image_coord_pts = NaN(num_vertex, 2, num_images);
+image_coord_pts = NaN(num_vertex, 2);
 % Iterate over files
 for i = 1:num_images
     currentImage = imread(fullfile(path_images,dir_images(i).name));
@@ -27,15 +27,25 @@ for i = 1:num_images
     [height, width, ~]  = size(currentImage);
     
     num_points = length(x);
+    cpy_ply_vertex = ply_vertex_coord;
     % Save coordinates of points
     for j = 1:num_vertex
         % Points inside image boundaries, if not, set to -1
         if ((x(j) <= width) && (x(j) >= 0)) && ((y(j) <= height)&& (y(j) >= 0)) 
-            image_coord_pts(j,1,i) = x(j);
-            image_coord_pts(j,2,i) = y(j);
-        else
-            image_coord_pts(j,1,i) = -1;
-            image_coord_pts(j,2,i) = -1;
+            image_coord_pts(j,1) = x(j);
+            image_coord_pts(j,2) = y(j);
+        else 
+            image_coord_pts(j,1) = -1;
+            image_coord_pts(j,2) = -1;
+        end
+    end
+    image_coord_cpy = image_coord_pts;
+    num_delete = 0;
+    for k = 1:num_vertex
+        if(image_coord_pts(k,1) == -1)
+            image_coord_cpy(k - num_delete, :) = [];
+            cpy_ply_vertex(k - num_delete, :) = [];
+            num_delete = num_delete +1;
         end
     end
     close(fig);

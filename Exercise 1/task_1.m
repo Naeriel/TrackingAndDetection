e41480d206bcfa15080c24ffde83e67adfd00e2e
Cristina <serranog.cristina@gmail.com>
2018-11-20 19:20:31 +0100

    minor fixes pt1

--- Exercise 1/task_1.m
+++ Exercise 1/task_1.m
@@ -77,48 +77,48 @@ hold off
 %% SIFT Keypoints
 loc1 = loc_matrix(1,1:3);
 ori1 = orient_matrix(1:3,1:3);
-scatter_points_merged = scatterPoints('DSC_9743.JPG',ori1,loc1,intrinsicsMat',ply_vertex_coord,ply_faces,[5,6,11,12]);
+scatter_points_merged = projectPoints('DSC_9743.JPG',ori1,loc1,intrinsicsMat',ply_vertex_coord,ply_faces,[5,6,11,12]);
 
 loc2 = loc_matrix(2,1:3);
 ori2 = orient_matrix(4:6,1:3);
-scatter_points = scatterPoints('DSC_9744.JPG',ori2,loc2,intrinsicsMat',ply_vertex_coord,ply_faces,[5,6,11,12,4,3]);
+scatter_points = projectPoints('DSC_9744.JPG',ori2,loc2,intrinsicsMat',ply_vertex_coord,ply_faces,[5,6,11,12,4,3]);
 scatter_points_merged = [scatter_points_merged;scatter_points];
 scatter3(scatter_points(:,1),scatter_points(:,2),scatter_points(:,3));
 2
 
 loc3 = loc_matrix(3,1:3);
 ori3 = orient_matrix(7:9,1:3);
-scatter_points = scatterPoints('DSC_9745.JPG',ori3,loc3,intrinsicsMat',ply_vertex_coord,ply_faces,[6,5,4,3]);
+scatter_points = projectPoints('DSC_9745.JPG',ori3,loc3,intrinsicsMat',ply_vertex_coord,ply_faces,[6,5,4,3]);
 scatter_points_merged = [scatter_points_merged;scatter_points];
 3
 
 loc4 = loc_matrix(4,1:3);
 ori4 = orient_matrix(10:12,1:3);
-scatter_points = scatterPoints('DSC_9746.JPG',ori4,loc4,intrinsicsMat',ply_vertex_coord,ply_faces,[4,3,5,6,1,2]);
+scatter_points = projectPoints('DSC_9746.JPG',ori4,loc4,intrinsicsMat',ply_vertex_coord,ply_faces,[4,3,5,6,1,2]);
 scatter_points_merged = [scatter_points_merged;scatter_points];
 4
 
 loc5 = loc_matrix(5,1:3);
 ori5 = orient_matrix(13:15,1:3);
-scatter_points = scatterPoints('DSC_9747.JPG',ori5,loc5,intrinsicsMat',ply_vertex_coord,ply_faces,[1,2,5,6]);
+scatter_points = projectPoints('DSC_9747.JPG',ori5,loc5,intrinsicsMat',ply_vertex_coord,ply_faces,[1,2,5,6]);
 scatter_points_merged = [scatter_points_merged;scatter_points];
 5
 
 loc6 = loc_matrix(6,1:3);
 ori6 = orient_matrix(16:18,1:3);
-scatter_points = scatterPoints('DSC_9748.JPG',ori6,loc6,intrinsicsMat',ply_vertex_coord,ply_faces,[1,2,6,7,5,6]);
+scatter_points = projectPoints('DSC_9748.JPG',ori6,loc6,intrinsicsMat',ply_vertex_coord,ply_faces,[1,2,6,7,5,6]);
 scatter_points_merged = [scatter_points_merged;scatter_points];
 6
 
 loc7 = loc_matrix(7,1:3);
 ori7 = orient_matrix(19:21,1:3);
-scatter_points = scatterPoints('DSC_9749.JPG',ori7,loc7,intrinsicsMat',ply_vertex_coord,ply_faces,[7,8,5,6]);
+scatter_points = projectPoints('DSC_9749.JPG',ori7,loc7,intrinsicsMat',ply_vertex_coord,ply_faces,[7,8,5,6]);
 scatter_points_merged = [scatter_points_merged;scatter_points];
 7
 
 loc8 = loc_matrix(8,1:3);
 ori8 = orient_matrix(22:24,1:3);
-scatter_points = scatterPoints('DSC_9750.JPG',ori8,loc8,intrinsicsMat',ply_vertex_coord,ply_faces,[7,8,11,12,5,6]);
+scatter_points = projectPoints('DSC_9750.JPG',ori8,loc8,intrinsicsMat',ply_vertex_coord,ply_faces,[7,8,11,12,5,6]);
 scatter_points_merged = [scatter_points_merged;scatter_points];
 8
 figure;

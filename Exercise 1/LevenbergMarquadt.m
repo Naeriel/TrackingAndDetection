326f177f6d29aec56d1c05d0cb086d6130ff2386
crystal-ship <mashec.s@gmail.com>
2018-12-10 20:38:24 +0200

    Update LevenbergMarquadt.m

--- Exercise 1/LevenbergMarquadt.m
+++ Exercise 1/LevenbergMarquadt.m
@@ -1,24 +1,67 @@
-%% Compute LM algorithm
-function theta = LevenbergMarquadt(x,theta0, N, thresh)
-    theta = theta0;
+function [Rt, Tt] = LevenbergMarquadt(R0, T0, A, M, m,  N, thresh)
+    
+    R = R0;
+    T = T0;
+    
+    
     lambda = 0.001;
     u = thresh + 1;
     
-    for ((i = 1:N) && (u > thresh))
-        J = jacobian(f, v);
-        e = energy(x, theta);
+    for i = 1:N
+      if (u > thresh)
+          
+        v = rotationMatrixToVector(R); %rotationVector
         
-        delta = -(J'*J + lambda*J)^(-1)*(J'*e)
-        en = energy(x, theta + delta);
         
-        if en > e
+        theta = [v, T]';
+        
+        vx = skewSymmetric (v);
+
+     %   tmp = cross(v, eye(3)-R);
+        
+   %     dR_dv1 = (v(1)*vx+m(1))*R/sum(v.^2);
+        dR_dv = (cross(v,vx)+cross(v, eye(3)-R))*R/sum(v.^2);
+        J = [];
+        for j=1:size(M)
+            dM_dp = [dR_dv*M(j,:), eye(3)];
+            dmt_dM = A;
+            mt = A*(R*M(j,:)'+T'); %feature point backprojected on image
+            U = mt(1);
+            V = mt(2);
+            W = mt(3);
+            
+            dm_dmt = [1/W, 0, -U/W^2; 0 , 1/W, -V/W^2];
+            
+            J_new = dm_dmt * dmt_dM * dM_dp;
+            J = [J;J_new];
+        end
+
+        
+        e = energy(R, T, A, M, m);
+        
+        delta = -(J'*J + lambda*J)^(-1)*(J'*e);
+        
+        theta_new = theta + delta;
+        v_new = theta_new (1:3);
+        R_new = rotationVectorToMatrix (v_new);
+        T_new = theta_new (4:6);
+        e_new = energy(R_new, T_new, A, M, m);
+        
+        if e_new > e
             lambda = 10 * lambda;
         else
             lambda = 10 / lambda;
-            theta = theta + delta;
-        end
-        
+            R = R_new;
+            T = T_new;
+        end                  
         u = norm(delta);
+      
+      else 
+          break;
+      end
+      
+      Rt = R;
+      Tt = T;
+      
     end
 end
-

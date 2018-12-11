6ad221e02bb9181bfd19d474922568ceff2f11b5
Cristina <serranog.cristina@gmail.com>
2018-12-11 16:35:14 +0100

    change energy function

--- Exercise 1/energyFunction.m
+++ Exercise 1/energyFunction.m
@@ -1,7 +1,5 @@
 %% Energy function F to compute camera pose
-function E = energyFunction(R, T, A, M, m, angle_fun)
+function E = energyFunction(R, T, A, M, m)
     
-    M = (M * angle_fun(R)' + repmat(T', [size(M, 1), 1])) * A'; %compute Matrix M   
-    dist = normalize(M) - m; % reprojection error
-    E = sum(sum(dist.^2));
+    E = sum(sum(dot(A,R.*M+T)-m)^2);
 end
